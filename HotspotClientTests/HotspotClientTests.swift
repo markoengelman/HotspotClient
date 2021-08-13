34476b1f6509cf409871f3be6fac1f486a803d03
Marko Engelman <engelman.marko@gmail.com>
2021-08-13 21:58:17 +0200

    Disconnect removes configuration.

--- HotspotClientTests/HotspotClientTests.swift
+++ HotspotClientTests/HotspotClientTests.swift
@@ -24,7 +24,7 @@ class NEHotspotClient: HotspotClient {
   }
   
   func disconnect(from SSID: String) {
-    
+    hotspotManager.removeConfiguration(forSSID: SSID)
   }
 }
 
@@ -37,17 +37,38 @@ class HotspotClientTests: XCTestCase {
   
   func test_connect_eventuallyDeliversResult() {
     let sut = makeSUT()
-    let configuration = HotspotConfiguration(ssid: "anySSID", password: "anyPassword", isWEP: false)
     let exp = expectation(description: "Waiting for result")
-    sut.connect(with: configuration) { result in
+    sut.connect(with: anyConfiguration) { result in
       exp.fulfill()
     }
     wait(for: [exp], timeout: 1.0)
+  }
+  
+  func test_disconnect_removesConfiguration() {
+    let sut = makeSUT()
+    let configuration = anyConfiguration
+    let exp = expectation(description: "Waiting for result")
+    
+    var SSIDs: [String] = []
+    sut.connect(with: configuration) { _ in
+      sut.disconnect(from: configuration.ssid)
+      sut.hotspotManager.getConfiguredSSIDs { configuredSSIDs in
+        SSIDs = configuredSSIDs
+        exp.fulfill()
+      }
+    }
+    
+    wait(for: [exp], timeout: 1.0)
+    XCTAssertFalse(SSIDs.contains(configuration.ssid))
   }
 }
 
 // MARK: - Private
 private extension HotspotClientTests {
+  var anyConfiguration: HotspotConfiguration {
+    HotspotConfiguration(ssid: "anySSID", password: "anyPassword", isWEP: false)
+  }
+  
   func makeSUT(manager: NEHotspotConfigurationManager = .shared) -> NEHotspotClient {
     let sut = NEHotspotClient(hotspotManager: manager)
     return sut
