eca3ef3f4870014d309120d096322bde94f15cc8
Marko Engelman <engelman.marko@gmail.com>
2021-08-14 11:56:08 +0200

    Add and use 'compyInterfaces' for older iOS versions.

--- HotspotClient/SSIDLoader/Infrastructure/SystemSSIDLoader.swift
+++ HotspotClient/SSIDLoader/Infrastructure/SystemSSIDLoader.swift
@@ -6,9 +6,21 @@
 //
 
 import Foundation
+import SystemConfiguration.CaptiveNetwork
 import NetworkExtension
 
 class SystemSSIDLoader {
+  @available(iOS, deprecated: 14.0, obsoleted: 14.0, message: "API is deprecated and obsoleted in iOS 14.0, use 'fetchCurrentNetwork' instead.")
+  func compyInterfaces() -> [String] {
+    guard let interfaceNames = CNCopySupportedInterfaces() as? [String] else { return [] }
+    let infterfaces = interfaceNames.compactMap { name -> String? in
+        guard let info = CNCopyCurrentNetworkInfo(name as CFString) as? [String: AnyObject] else { return nil }
+        guard let ssid = info[kCNNetworkInfoKeySSID as String] as? String else { return nil }
+        return ssid
+    }
+    return infterfaces
+  }
+  
   @available(iOS 14.0, *)
   func fetchCurrentNetwork(completion: @escaping ([String]) -> Void) {
     NEHotspotNetwork.fetchCurrent { network in
@@ -22,7 +34,9 @@ class SystemSSIDLoader {
 extension SystemSSIDLoader: SSIDLoader {
   func load(completion: @escaping ([String]) -> Void) {
     if #available(iOS 14.0, *) {
-        fetchCurrentNetwork(completion: completion)
+      fetchCurrentNetwork(completion: completion)
+    } else {
+      completion(compyInterfaces())
     }
   }
 }
--- HotspotClientTests/SSIDLoader/SystemSSIDLoaderTests.swift
+++ HotspotClientTests/SSIDLoader/SystemSSIDLoaderTests.swift
@@ -9,29 +9,51 @@ import XCTest
 @testable import HotspotClient
 
 class SystemSSIDLoaderTests: XCTestCase {
-  func test_load_uses_iOS14API_ifAvailable() {
+  func test_load_uses_iOS14API_ifAvailable() throws {
+    guard #available(iOS 14.0, *) else {
+      throw XCTSkip("This test is designed for latest iOS api only.")
+    }
     let sut = makeSUT()
     let exp = expectation(description: "Expecting to call iOS 14 api")
     sut.load { _ in exp.fulfill() }
     wait(for: [exp], timeout: 1.0)
     XCTAssertTrue(sut.iOS14APIUsed)
+    XCTAssertFalse(sut.legacyAPIUsed)
+  }
+  
+  func test_load_uses_legacyAPI_onOlderiOSVersions() throws {
+    let version = Double(UIDevice.current.systemVersion)!
+    let isTestAvailable = version < 14.0
+    try XCTSkipIf(!isTestAvailable , "This test is not designed for iOS newer or equalt to 14.0")
+    let sut = makeSUT()
+    let exp = expectation(description: "Expecting to call legacy api")
+    sut.load { _ in exp.fulfill() }
+    wait(for: [exp], timeout: 1.0)
+    XCTAssertTrue(sut.legacyAPIUsed)
+    XCTAssertFalse(sut.iOS14APIUsed)
   }
 }
 
 // MARK: - Private
 private extension SystemSSIDLoaderTests {
-  class SystemSSIDLoaderAnalyzer: SystemSSIDLoader {
+  class SystemSSIDLoaderMock: SystemSSIDLoader {
     var iOS14APIUsed: Bool = false
+    var legacyAPIUsed: Bool = false
     
     @available(iOS 14.0, *)
     override func fetchCurrentNetwork(completion: @escaping ([String]) -> Void) {
       iOS14APIUsed = true
       super.fetchCurrentNetwork(completion: completion)
     }
+    
+    override func compyInterfaces() -> [String] {
+      legacyAPIUsed = true
+      return super.compyInterfaces()
+    }
   }
   
-  func makeSUT() -> SystemSSIDLoaderAnalyzer {
-    let sut = SystemSSIDLoaderAnalyzer()
+  func makeSUT() -> SystemSSIDLoaderMock {
+    let sut = SystemSSIDLoaderMock()
     return sut
   }
 }
