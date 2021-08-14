b4cfcd1acd20af574e585cb245e44b75d147e941
Marko Engelman <engelman.marko@gmail.com>
2021-08-14 12:00:50 +0200

    Add 'HotspotClientWithValidationTests'.

---
+++ HotspotClientTests/HotspotClientWithValidationTests.swift
@@ -0,0 +1,12 @@
+//
+//  HotspotClientWithValidationTests.swift
+//  HotspotClientTests
+//
+//  Created by Marko Engelman on 14/08/2021.
+//
+
+import XCTest
+
+class HotspotClientWithValidationTests: XCTestCase {
+  
+}
