b3f991fc7b3c0e8d657ec2f61c3b87a6c56a2c8c
Marko Engelman <engelman.marko@gmail.com>
2021-08-14 10:48:58 +0200

    Use iOS14 'NEHotspotNetwork.fetchCurrent' to deliver current SSID info.

--- HotspotClient/SSIDLoader/Infrastructure/SystemSSIDLoader.swift
+++ HotspotClient/SSIDLoader/Infrastructure/SystemSSIDLoader.swift
@@ -6,14 +6,23 @@
 //
 
 import Foundation
+import NetworkExtension
 
 class SystemSSIDLoader {
-  
+  @available(iOS 14.0, *)
+  func fetchCurrentNetwork(completion: @escaping ([String]) -> Void) {
+    NEHotspotNetwork.fetchCurrent { network in
+        guard let network = network else { return completion([]) }
+        completion([network.ssid])
+    }
+  }
 }
 
 // MARK: - SSIDLoader
 extension SystemSSIDLoader: SSIDLoader {
   func load(completion: @escaping ([String]) -> Void) {
-    
+    if #available(iOS 14.0, *) {
+        fetchCurrentNetwork(completion: completion)
+    }
   }
 }
--- HotspotClientTests/SSIDLoader/SystemSSIDLoaderTests.swift
+++ HotspotClientTests/SSIDLoader/SystemSSIDLoaderTests.swift
@@ -9,13 +9,29 @@ import XCTest
 @testable import HotspotClient
 
 class SystemSSIDLoaderTests: XCTestCase {
-
+  func test_load_uses_iOS14API_ifAvailable() {
+    let sut = makeSUT()
+    let exp = expectation(description: "Expecting to call iOS 14 api")
+    sut.load { _ in exp.fulfill() }
+    wait(for: [exp], timeout: 1.0)
+    XCTAssertTrue(sut.iOS14APIUsed)
+  }
 }
 
 // MARK: - Private
 private extension SystemSSIDLoaderTests {
-  func makeSUT() -> SystemSSIDLoader {
-    let sut = SystemSSIDLoader()
+  class SystemSSIDLoaderAnalyzer: SystemSSIDLoader {
+    var iOS14APIUsed: Bool = false
+    
+    @available(iOS 14.0, *)
+    override func fetchCurrentNetwork(completion: @escaping ([String]) -> Void) {
+      iOS14APIUsed = true
+      super.fetchCurrentNetwork(completion: completion)
+    }
+  }
+  
+  func makeSUT() -> SystemSSIDLoaderAnalyzer {
+    let sut = SystemSSIDLoaderAnalyzer()
     return sut
   }
 }
