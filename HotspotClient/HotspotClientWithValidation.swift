1ee72fed56cded12f65d03a3ccc0671092f69bfa
Marko Engelman <engelman.marko@gmail.com>
2021-08-14 11:58:51 +0200

    Add 'HotspotClientWithValidation'.

---
+++ HotspotClient/HotspotClientWithValidation.swift
@@ -0,0 +1,12 @@
+//
+//  HotspotClientWithValidation.swift
+//  HotspotClient
+//
+//  Created by Marko Engelman on 14/08/2021.
+//
+
+import Foundation
+
+class HotspotClientWithValidation {
+  
+}
